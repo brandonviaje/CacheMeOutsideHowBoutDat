bae79a0da90bed4946f32910bb30320e8f68f6af
Brandon <viajebrandon@gmail.com>
2026-01-10 00:51:53 -0500

    feat: implement incremental hashmap, with insert,lookup and delete

---
+++ src/hashmap.cpp
@@ -0,0 +1,106 @@
+#include "hashmap.h"
+
+void hashmap_migrate_keys(HashMap* hashmap)
+{
+    std::size_t nwork {};       // keep track of num of nodes moved so far
+
+    while(nwork < REHASHING_WORK && hashmap->older.size > 0)
+    {   
+        // find an occupied slot
+        HashNode** from {&hashmap->older.table[hashmap->migrate_position]};
+
+        if(!*from)
+        {
+            hashmap->migrate_position++;
+            continue;   // empty slot, continue
+        }
+
+        // move first node from older to newer table, update num of nodes moved
+        hash_node_insert(&hashmap->newer, hash_node_delete(&hashmap->older,from));
+        nwork++;
+    }
+
+    // delete old table if migration done
+    if(hashmap->older.size == 0 && hashmap->older.table)
+    {
+        free(hashmap->older.table);
+        hashmap->older = HashTable{};
+    }
+}
+
+void hashmap_trigger_rehashing(HashMap* hashmap) 
+{
+    hashmap->older = hashmap->newer;                                    // older table now the prev new table
+    hash_table_init(&hashmap->newer, (hashmap->newer.mask + 1) * 2);    // double size for new table
+    hashmap->migrate_position = 0;                                      // reset migration index
+}
+
+// GET,SET,DEL interfaces
+
+HashNode* hashmap_lookup(HashMap *hashmap, HashNode *key, bool (*eq)(HashNode *, HashNode *))
+{
+    // query newer table first
+    HashNode** from {hash_node_lookup(&hashmap->newer, key, eq)};
+
+    if (from && *from)
+    {
+        return *from;
+    }
+
+    // query older table if not found in newer table
+    from = hash_node_lookup(&hashmap->older, key, eq);
+
+    if (from && *from)
+    {
+        return *from;
+    }
+        
+    return nullptr; // not found
+}
+
+void hashmap_insert(HashMap *hashmap, HashNode *node)
+{  
+
+    // init newer table if empty
+    if(!hashmap->newer.table)
+    {
+        hash_table_init(&hashmap->newer, 4);
+    }
+
+    hash_node_insert(&hashmap->newer, node);    // insertion should update the newer table
+
+    // check if we need to rehash (load factor is high)
+    if(!hashmap->older.table)
+    {
+        std::size_t threshold {(hashmap->newer.mask+1) * MAX_LOAD_FACTOR};
+
+        if(hashmap->newer.size >= threshold)
+        {
+            hashmap_trigger_rehashing(hashmap);
+        }
+    }
+
+    // migrate keys
+    hashmap_migrate_keys(hashmap);
+}
+
+HashNode* hashmap_delete(HashMap *hashmap, HashNode *key, bool (*eq)(HashNode *, HashNode *))
+{
+    // query newer table first
+    HashNode** from {hash_node_lookup(&hashmap->newer, key, eq)};
+
+    if (from && *from)
+    {
+        return hash_node_delete(&hashmap->newer, from);
+    }
+    
+    // query older table next if not found in newer table
+    from = hash_node_lookup(&hashmap->older, key, eq);
+
+    if (from && *from)
+    {
+        return hash_node_delete(&hashmap->older, from);
+    }
+
+    return nullptr; // not found
+}
---
+++ src/hashmap.h
@@ -0,0 +1,22 @@
+#ifndef HASHMAP_H
+#define HASHMAP_H
+
+#include "hashtable.h"
+
+const std::size_t MAX_LOAD_FACTOR = 8;
+const std::size_t REHASHING_WORK = 128;
+
+struct HashMap
+{
+    HashTable newer;
+    HashTable older;
+    std::size_t migrate_position{};
+};
+
+void hashmap_migrate_keys(HashMap* hashmap);
+void hashmap_trigger_rehashing(HashMap* hashmap);
+HashNode* hashmap_lookup(HashMap *hashmap, HashNode *key, bool (*eq)(HashNode *, HashNode *));
+void hashmap_insert(HashMap *hashmap, HashNode *node);
+HashNode* hashmap_delete(HashMap *hashmap, HashNode *key, bool (*eq)(HashNode *, HashNode *));
+
+#endif
